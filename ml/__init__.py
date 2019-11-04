152283f6d302d80ad77e13f8c1ddc10ddc5a0fe7
devshilei <devshilei@163.com>
2019-11-04 20:52:19 +0800

    ml-机器学习相关模型实现代码存放位置。 nn-深度神经网络实现代码存放位置。

---
+++ ml/__init__.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-#
+
+#--------------------------------
+# Name:         __init__.py.py
+# Author:       devshilei@gmail.com
+# Description:  
+#--------------------------------
